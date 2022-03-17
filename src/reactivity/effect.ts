8f02fd6416d597f5474836a150450ac75e5c6736
Avan <layouwen@gmail.com>
2022-03-17 16:02:48 +0800

    feat: add scheduler to effect

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,6 +1,6 @@
 class ReactiveEffect {
   private _fn: any
-  constructor(fn) {
+  constructor(fn, public scheduler?) {
     this._fn = fn
   }
   run() {
@@ -9,8 +9,8 @@ class ReactiveEffect {
   }
 }
 
-export function effect(fn) {
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn, options: any = {}) {
+  const _effect = new ReactiveEffect(fn, options.scheduler)
   _effect.run()
   return _effect.run.bind(_effect)
 }
@@ -36,6 +36,10 @@ export function trigger(target, key) {
   const depsMap = targetMap.get(target)
   const deps = depsMap.get(key)
   for (const effect of deps) {
-    effect.run()
+    if (effect.scheduler) {
+      effect.scheduler()
+    } else {
+      effect.run()
+    }
   }
 }
