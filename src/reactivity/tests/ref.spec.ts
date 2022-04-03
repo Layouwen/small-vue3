b4022ce34b191b39d77f0b78d2c091a7aa170522
Avan <layouwen@gmail.com>
2022-04-03 18:18:43 +0800

    feat: add isRef and unRef function

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,6 @@
 import { effect } from '../effect'
-import { ref } from '../ref'
+import { reactive } from '../reactive'
+import { isRef, ref, unRef } from '../ref'
 describe('ref', () => {
   it('happy path', () => {
     const a = ref(1)
@@ -35,5 +36,19 @@ describe('ref', () => {
     expect(dummy).toBe(1)
     a.value.count = 2
     expect(dummy).toBe(2)
+  })
+
+  it('isRef', () => {
+    const a = ref(1)
+    const bank = reactive({ money: 100 })
+    expect(isRef(a)).toBe(true)
+    expect(isRef(100)).toBe(false)
+    expect(isRef(bank)).toBe(false)
+  })
+
+  it('unRef', () => {
+    const a = ref(1)
+    expect(unRef(a)).toBe(1)
+    expect(unRef(100)).toBe(100)
   })
 })
