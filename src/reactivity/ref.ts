26ac0717e99a7d36050908840bead516a91e9ee2
Avan <layouwen@gmail.com>
2022-04-03 18:34:19 +0800

    feat: add proxyRefs function

--- src/reactivity/ref.ts
+++ src/reactivity/ref.ts
@@ -48,3 +48,22 @@ export function isRef(ref) {
 export function unRef(ref) {
   return isRef(ref) ? ref.value : ref
 }
+
+export function proxyRefs(objectWithRefs) {
+  return new Proxy(objectWithRefs, {
+    get(target, key) {
+      // If you get the value of a ref, it returns its .value. If it's not ref, it returns itself.
+      // 如果获取一个 ref 的值，就返回它的 .value 。如果不是 ref 这返回本身
+      return unRef(Reflect.get(target, key))
+    },
+    set(target, key, value) {
+      // If you are settings a normal value, set it directly. If Ref, set its value.
+      // 如果设置的是普通值，直接设置。如果是 ref，就设置它的 .value
+      if (isRef(target[key]) && !isRef(value)) {
+        return (target[key].value = value)
+      } else {
+        return Reflect.set(target, key, value)
+      }
+    },
+  })
+}
